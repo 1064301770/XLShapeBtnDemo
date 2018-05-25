3c1f9d0bff35613ad886a033fcdc09c86320da88
huxiaolong <1064301770@qq.com>
2018-05-25 14:06:56 +0800

    一个不规则的按钮

---
+++ XLShapeBtnDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XLShapeBtn
+//
+//  Created by 蚁族 on 2018/5/24.
+//  Copyright © 2018年 蚁族. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XLShapeBtnDemo/XLCompelShapeController.h
@@ -0,0 +1,13 @@
+//
+//  XLCompelShapeController.h
+//  XLShapeBtn
+//
+//  Created by 蚁族 on 2018/5/24.
+//  Copyright © 2018年 mooyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XLCompelShapeController : UIViewController
+
+@end
